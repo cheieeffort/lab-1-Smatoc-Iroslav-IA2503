d3d2cda679c7b2f125733a1ca738ff40d06d504c
cheieeffort <Earik.vodila@gmail.com>
2026-02-11 10:34:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+alert("Этот код выполнен из внешнего файла!");
+console.log("Сообщение в консоли");
+name = prompt ("Введите ваше имя");
+birthYear = prompt ("Введите ваш год рождения");
+isStuent = confirm ("Вы студент?");
+
+let score = prompt("Введите ваш балл:");
+if (score >= 90) {
+ console.log("Отлично!");
+} else if (score >= 70) {
+ console.log("Хорошо");
+} else {
+ console.log("Можно лучше!");
+}
+
+for (let i = 1; i <= 5; i++) {
+ console.log(`Итерация: ${i}`);
+}
